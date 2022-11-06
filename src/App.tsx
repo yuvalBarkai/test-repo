e33f655d755fb33710b2ddcef9b9409649ff9da6
yuvalBarkai <yuvbarkai@gmail.com>
2022-11-06 11:58:24 +0200

    second version of this test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <h1>React Template</h1>
+            TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+            tttt
         </div>
     );
 }
